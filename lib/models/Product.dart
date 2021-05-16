69e4d9e799b1ba0268724bebb586b1f7c8f12009
noxion1 <43441327+noxion1@users.noreply.github.com>
2021-05-16 17:49:44 +0200

    Mobile
    Start Mobile version for Bizander.
    
    Added: Store and Details page

---
+++ lib/models/Product.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class Product {
+  final String image, title, description;
+  final int price, size, id;
+
+  Product({
+    this.id,
+    this.image,
+    this.title,
+    this.price,
+    this.description,
+    this.size,
+
+  });
+}
+
+List<Product> products = [
+  Product(
+      id: 1,
+      title: "Office Code",
+      price: 234,
+      size: 12,
+      description: dummyText,
+      image: "assets/images/tas_1.png",
+      ),
+  Product(
+      id: 2,
+      title: "Belt Bag",
+      price: 234,
+      size: 8,
+      description: dummyText,
+      image: "assets/images/tas_1.png",
+      ),
+  Product(
+      id: 3,
+      title: "Hang Top",
+      price: 234,
+      size: 10,
+      description: dummyText,
+      image: "assets/images/tas_1.png",
+      ),
+  Product(
+      id: 4,
+      title: "Old Fashion",
+      price: 234,
+      size: 11,
+      description: dummyText,
+      image: "assets/images/tas_1.png",
+      ),
+  Product(
+      id: 5,
+      title: "Office Code",
+      price: 234,
+      size: 12,
+      description: dummyText,
+      image: "assets/images/tas_1.png",
+      ),
+  Product(
+    id: 6,
+    title: "Office Code",
+    price: 234,
+    size: 12,
+    description: dummyText,
+    image: "assets/images/tas_1.png",
+
+  ),
+];
+
+String dummyText =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since. When an unknown printer took a galley.";
---
+++ lib/screens/details/components/body.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/constants.dart';
+import 'package:shop_app/models/Product.dart';
+
+import 'add_to_cart.dart';
+import 'description.dart';
+import 'product_title_with_image.dart';
+
+class Body extends StatelessWidget {
+  final Product product;
+
+  const Body({Key key, this.product}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // It provide us total height and width
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: size.height,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: size.height * 0.4),
+                  padding: EdgeInsets.only(
+                    top: size.height * 0.10,
+                    left: kDefaultPaddin,
+                    right: kDefaultPaddin,
+                  ),
+                  // height: 500,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(24),
+                      topRight: Radius.circular(24),
+                    ),
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: kDefaultPaddin / 2),
+                      Description(product: product),
+                      SizedBox(height: kDefaultPaddin / 2),
+                      SizedBox(height: kDefaultPaddin / 2),
+                      AddToCart(product: product)
+                    ],
+                  ),
+                ),
+                ProductTitleWithImage(product: product)
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
